9113bc350be46f3951bb35748ff26f978efceaef
Timothy High <timothy.high@gmail.com>
2015-01-06 23:31:07 -0200

    Consertando response_xml de authorization

--- lib/komerci/authorization.rb
+++ lib/komerci/authorization.rb
@@ -3,8 +3,8 @@ require "nokogiri"
 
 module Komerci
   class Authorization
-    attr_accessor :code, :code_confirm, :order_number, :number, :receipt_number, :authentication_number, :sequential_number, :country_code
-    attr_reader :message, :message_confirm, :date, :response_xml
+    attr_accessor :code, :code_confirm, :order_number, :number, :receipt_number, :authentication_number, :sequential_number, :country_code, :response_xml
+    attr_reader :message, :message_confirm, :date
 
     def date=(value)
       @date = Date.parse(value) unless value.blank?
@@ -23,11 +23,9 @@ module Komerci
     end
 
     def self.from_xml(string)
-      @response_xml = string
-      puts string
-      
       xml = Nokogiri::XML(string)
       new.tap do |a|
+        a.response_xml          = string
         a.code                  = xml_text(xml, "CODRET")
         a.message               = xml_text(xml, "MSGRET")
         a.order_number          = xml_text(xml, "NUMPEDIDO")
